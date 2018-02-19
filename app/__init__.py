24fe3eb71ea4110bf41fbb8cb561cc38a94f8806
Camille Campbell <c.campbell1996@hotmail.com>
2018-02-19 01:44:53 +0000

    Submission of files

--- app/__init__.py
+++ app/__init__.py
@@ -5,8 +5,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY']= 'hot potatoes'
 app.config['MAIL_SERVER']= 'smtp.mailtrap.io'
 app.config['MAIL_PORT']= '2525'
-app.config['MAIL_USERNAME']= 'fa7d0625113049'
-app.config['MAIL_PASSWORD']= '0623d337bf6c72'
+
 
 mail=Mail(app)
 from app import views
